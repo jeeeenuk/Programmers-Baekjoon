2ae0c81d11af1f6dc2ea5c56ec0491f89b5e09c2
Jinwook Jung <91586765+jeeeenuk@users.noreply.github.com>
2025-03-19 17:09:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        long sum = a+b+c;
+        System.out.println(sum);
+        sc.close();
+    }
+}
