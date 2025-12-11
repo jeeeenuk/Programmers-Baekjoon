82349193f53ee65bd2c03226abf5c6d3c228cd5c
Jinwook Jung <91586765+jeeeenuk@users.noreply.github.com>
2025-12-11 21:25:14 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N/4; i++) {
+            System.out.print("long ");
+        }
+        System.out.print("int");
+        bw.close();
+    }
+}
