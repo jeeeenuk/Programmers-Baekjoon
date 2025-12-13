ca7032c192bd7570932f8a633c70f946a53f76a6
Jinwook Jung <91586765+jeeeenuk@users.noreply.github.com>
2025-12-13 20:31:40 +0900

    [Bronze II] Title: 상수, Time: 112 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -4,20 +4,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] A = st.nextToken().split("");
-        String[] B = st.nextToken().split("");
-        String a = "";
-        String b = "";
-
-        for (int i = 2; i >= 0; i--) {
-            a += Integer.parseInt(A[i]);
-            b += Integer.parseInt(B[i]);
-        }
-        if (Integer.parseInt(a) > Integer.parseInt(b)){
-            System.out.print(a);
-        } else System.out.print(b);
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        System.out.println(Math.max(a, b));
 
         br.close();
     }
